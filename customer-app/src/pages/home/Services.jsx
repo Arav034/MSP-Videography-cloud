6036f62e2472367c7d9c30b44fecf015716d0eb6
Aravinth <aravinthmgt63@gmail.com>
2026-08-05 09:06:28 +0530

    Services gap updated

--- customer-app/src/pages/home/Services.jsx
+++ customer-app/src/pages/home/Services.jsx
@@ -13,7 +13,7 @@ export default function Services() {
         </p>
 
         <div className="mt-12 grid grid-cols-4
-        sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        sm:grid-cols-2 lg:grid-cols-4 gap-4">
           {SERVICE_CATEGORIES.map((cat) => (
             <ServiceCategoryCard key={cat.id} {...cat} />
           ))}
