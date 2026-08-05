d4c958934818b09f034c20d1b8e8d424865d16bb
Arav034 <aravinthmgt63@gmail.com>
2026-08-05 19:38:32 +0530

    removed console log

--- customer-app/src/services/supabase/supabaseClient.js
+++ customer-app/src/services/supabase/supabaseClient.js
@@ -1,11 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log(import.meta.env.VITE_SUPABASE_URL);
-console.log(import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-console.log(supabase);
