8bad3c914042cf71e85ab734b03106b7720c6c59
Aravinth <aravinthmgt63@gmail.com>
2026-08-08 14:42:23 +0530

    navbar white

--- customer-app/src/components/layout/Navbar.jsx
+++ customer-app/src/components/layout/Navbar.jsx
@@ -12,8 +12,8 @@ export default function Navbar() {
             to={item.to}
             end
             className={({ isActive }) =>
-                cn("nav-link font-nav block px-3 py-1", isActive ? "text-blue-700 font-semibold"
-                  : "hover:text-blue-800")
+                cn("nav-link font-nav block px-3 py-1", isActive  ? "text-white font-semibold"
+                  : "hover:text-white/80")
               }
           >
             {item.label}
